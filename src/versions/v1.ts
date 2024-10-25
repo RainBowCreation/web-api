814e24a2efde45a1fcf27fb1863ad3ee8081780e
VocanicZ <ampnattahapat@gmail.com>
2024-10-26 02:13:01 +0700

    add Enum getter

--- src/versions/v1.ts
+++ src/versions/v1.ts
@@ -1,4 +1,5 @@
 import { response } from "../api";
+import { STATUS, translateStatusMessage } from "../ENUM/STATUS";
 import { DataStore } from "../utils/DataStore";
 import { v0 } from "./v0";
 
@@ -15,4 +16,23 @@ export class v1 extends v0 {
       return response("v1");
     } catch (e) { this.logger.error('versions/v1.ts/getVersion', e) };
   }
+
+  async getEnum(params: { Enum: string, key: string }) {
+        try {
+            const { Enum, key } = params;
+            let value;
+            switch(Enum) {
+                case "STATUS": {
+                    value = translateStatusMessage(key);
+                    if (value !== -1) {
+                        return response({ key: key, Enum: Enum, value: value});
+                    }
+                    break;
+                }
+                
+            }
+            return response({ error: `${key}' not found` }, STATUS.BadRequest);
+        }
+        catch (e) { this.logger.error('BlinedSeek/api.ts/getConfig', e) };
+    }
 }
